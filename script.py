c4038c62b3a33249cbd0e8babe9fe07527d96c8b
imightmax <derooijmax@gmail.com>
2018-05-10 19:26:55 +0200

    Added a TODO

--- script.py
+++ script.py
@@ -7,6 +7,8 @@ soup_file = r.text
 soup = BeautifulSoup(soup_file, 'html.parser')
 kudos = soup.find_all('section', attrs={'id': 'top5'})
 
+#TODO
+#Vind de juiste selector voor de top5 lijst, extract alle kudos
 print(type(kudos))
 print(len(kudos))
 
